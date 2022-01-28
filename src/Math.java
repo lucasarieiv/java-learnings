760334db41774de74d881e1f77cd2fc8dd982dec
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2022-01-27 23:13:45 -0300

    First commit

---
+++ src/Math.java
@@ -0,0 +1,5 @@
+public class Math {
+    public static int soma(int v1, int v2) {
+        return v1 + v2;
+    }
+}
---
+++ src/outroNome.java
@@ -0,0 +1,5 @@
+class outroNome {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! \nMy name is Lucas");
+    }
+}
